29faaab726bebb89c5a2e54769eab2179198b3ed
Yingjie Zhang <yingjie_zhang@me.com>
2020-12-22 13:21:24 +0800

    add image sub cv_bridge

--- ROSgui.py
+++ ROSgui.py
@@ -10,6 +10,11 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 import rospy, time
 from std_msgs.msg import String
 
+from sensor_msgs.msg import Image
+from sensor_msgs.msg import CompressedImage
+from cv_bridge import CvBridge
+
+
 ### DECLARING VARIABLES ###
 global data, contS, contP, str_contS, str_contP
 data=0
@@ -59,73 +64,73 @@ class Ui_MainWindow(object):
         self.horizontalLayout_5.setObjectName("horizontalLayout_5")
         self.horizontalLayout = QtWidgets.QHBoxLayout()
         self.horizontalLayout.setObjectName("horizontalLayout")
-	### TITLE SUBSCRIBER ###
-	self.label_Sub = QtWidgets.QLabel(self.widget)
-	self.label_Sub.setObjectName("label_Sub")
-	### --- ###        
-	self.horizontalLayout.addWidget(self.label_Sub)
-	### DATA SUBSCRIBED (CHANGE OVER LOOP) ###        
-	self.label_Data_Sub = QtWidgets.QLabel(self.widget)
+        ### TITLE SUBSCRIBER ###
+        self.label_Sub = QtWidgets.QLabel(self.widget)
+        self.label_Sub.setObjectName("label_Sub")
+        ### --- ###        
+        self.horizontalLayout.addWidget(self.label_Sub)
+        ### DATA SUBSCRIBED (CHANGE OVER LOOP) ###        
+        self.label_Data_Sub = QtWidgets.QLabel(self.widget)
         self.label_Data_Sub.setObjectName("label_Data_Sub")
-	### --- ###        
-	self.horizontalLayout.addWidget(self.label_Data_Sub)
+        ### --- ###        
+        self.horizontalLayout.addWidget(self.label_Data_Sub)
         self.horizontalLayout_5.addLayout(self.horizontalLayout)
         self.horizontalLayout_4 = QtWidgets.QHBoxLayout()
         self.horizontalLayout_4.setObjectName("horizontalLayout_4")
-	### TITLE SUBSCRIBER COUNTER ###        
-	self.label_ContS = QtWidgets.QLabel(self.widget)
+        ### TITLE SUBSCRIBER COUNTER ###        
+        self.label_ContS = QtWidgets.QLabel(self.widget)
         self.label_ContS.setObjectName("label_ContS")
-	### --- ###        
-	self.horizontalLayout_4.addWidget(self.label_ContS)
-	### DATA CONT S (CHANGE OVER LOOP) ###        
-	self.label_Data_ContS = QtWidgets.QLabel(self.widget)
+        ### --- ###        
+        self.horizontalLayout_4.addWidget(self.label_ContS)
+        ### DATA CONT S (CHANGE OVER LOOP) ###        
+        self.label_Data_ContS = QtWidgets.QLabel(self.widget)
         self.label_Data_ContS.setObjectName("label_Data_ContS")
-	### --- ###	
-	self.horizontalLayout_4.addWidget(self.label_Data_ContS)
+        ### --- ###	
+        self.horizontalLayout_4.addWidget(self.label_Data_ContS)
         self.horizontalLayout_5.addLayout(self.horizontalLayout_4)
         self.verticalLayout.addLayout(self.horizontalLayout_5)
         self.horizontalLayout_6 = QtWidgets.QHBoxLayout()
         self.horizontalLayout_6.setObjectName("horizontalLayout_6")
         self.horizontalLayout_2 = QtWidgets.QHBoxLayout()
         self.horizontalLayout_2.setObjectName("horizontalLayout_2")
-	### TITLE PUBLISHER ###        
-	self.label_Pub = QtWidgets.QLabel(self.widget)
+        ### TITLE PUBLISHER ###        
+        self.label_Pub = QtWidgets.QLabel(self.widget)
         self.label_Pub.setObjectName("label_Pub")
-	### --- ###        
-	self.horizontalLayout_2.addWidget(self.label_Pub)
-	### DATA PUBLISHED (CHANGE OVER TIME) ###
+        ### --- ###        
+        self.horizontalLayout_2.addWidget(self.label_Pub)
+        ### DATA PUBLISHED (CHANGE OVER TIME) ###
         self.label_Data_Pub = QtWidgets.QLabel(self.widget)
         self.label_Data_Pub.setObjectName("label_Data_Pub")
-	### --- ###        
-	self.horizontalLayout_2.addWidget(self.label_Data_Pub)
+        ### --- ###        
+        self.horizontalLayout_2.addWidget(self.label_Data_Pub)
         self.horizontalLayout_6.addLayout(self.horizontalLayout_2)
         self.horizontalLayout_3 = QtWidgets.QHBoxLayout()
         self.horizontalLayout_3.setObjectName("horizontalLayout_3")
-	### TITLE PUBLISHER COUNTER ###        
-	self.label_ContP = QtWidgets.QLabel(self.widget)
+        ### TITLE PUBLISHER COUNTER ###        
+        self.label_ContP = QtWidgets.QLabel(self.widget)
         self.label_ContP.setObjectName("label_ContP")
-	### --- ###
+        ### --- ###
         self.horizontalLayout_3.addWidget(self.label_ContP)
-	### DATA CONT P (CHANGE OVER LOOP) ###        
-	self.label_Data_ContP = QtWidgets.QLabel(self.widget)
+        ### DATA CONT P (CHANGE OVER LOOP) ###        
+        self.label_Data_ContP = QtWidgets.QLabel(self.widget)
         self.label_Data_ContP.setObjectName("label_Data_ContP")
-	### --- ###
+        ### --- ###
         self.horizontalLayout_3.addWidget(self.label_Data_ContP)
         self.horizontalLayout_6.addLayout(self.horizontalLayout_3)
         self.verticalLayout.addLayout(self.horizontalLayout_6)
         self.verticalLayout_2.addLayout(self.verticalLayout)
         self.horizontalLayout_7 = QtWidgets.QHBoxLayout()
         self.horizontalLayout_7.setObjectName("horizontalLayout_7")
-	### PUBLISHER BUTTON ###        
-	self.pushButton_Pub = QtWidgets.QPushButton(self.widget)
+        ### PUBLISHER BUTTON ###        
+        self.pushButton_Pub = QtWidgets.QPushButton(self.widget)
         self.pushButton_Pub.setObjectName("pushButton_Pub")
-	### --- ###        
-	self.horizontalLayout_7.addWidget(self.pushButton_Pub)
-	### RESET BUTTON ###        
-	self.pushButton_Reset = QtWidgets.QPushButton(self.widget)
+        ### --- ###        
+        self.horizontalLayout_7.addWidget(self.pushButton_Pub)
+        ### RESET BUTTON ###        
+        self.pushButton_Reset = QtWidgets.QPushButton(self.widget)
         self.pushButton_Reset.setObjectName("pushButton_Reset")
-	### --- ###        
-	self.horizontalLayout_7.addWidget(self.pushButton_Reset)
+        ### --- ###        
+        self.horizontalLayout_7.addWidget(self.pushButton_Reset)
         self.verticalLayout_2.addLayout(self.horizontalLayout_7)
         self.verticalLayout_4.addLayout(self.verticalLayout_2)
         MainWindow.setCentralWidget(self.centralwidget)
@@ -139,7 +144,7 @@ class Ui_MainWindow(object):
 
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
-
+        self.bridge = CvBridge()            
     def retranslateUi(self, MainWindow):
 	### GUI MODIFICATIONS ###
         _translate = QtCore.QCoreApplication.translate
@@ -157,52 +162,57 @@ class Ui_MainWindow(object):
         self.pushButton_Reset.setText(_translate("MainWindow", "Reset"))
 	
 	### DECLARING FUNCTIONS ###
-	self.subCallback(data)
-	self.listener()
-	self.pushButton_Reset.clicked.connect(self.reset)
-	self.pushButton_Pub.clicked.connect(self.publish)
+        self.subCallback(data)
+        self.listener()
+        self.pushButton_Reset.clicked.connect(self.reset)
+        self.pushButton_Pub.clicked.connect(self.publish)
 	
     def subCallback(self,data):
-	global contS, str_contS
-	contS=contS+1
-	str_contS=str(contS)
-	text_sub=str(data)
-	self.label_Data_Sub.setText(text_sub)
-	self.label_Data_ContS.setText(str_contS)
-    
+        global contS, str_contS
+        contS=contS+1
+        str_contS=str(contS)
+        text_sub=str(data)
+        self.label_Data_Sub.setText(text_sub)
+        self.label_Data_ContS.setText(str_contS)
+    def subImgCb(self, data):
+        pass
+        cv_image = self.bridge.imgmsg_to_cv2(data, desired_encoding='passthrough')
+        
+        print(cv_image.shape)
+
     def listener(self):
-	rospy.init_node('ROSgui', anonymous=True)
-	rospy.Subscriber('chatter', String, self.subCallback)
+        rospy.init_node('ROSgui', anonymous=True)
+        rospy.Subscriber('chatter', String, self.subCallback)
 
     def reset(self):
-	global contS, contP, str_contS
-	contS=0
-	contP=0
-	str_contS=str(contS)
-	str_contP=str(contP)
-	self.label_Data_ContS.setText(str_contS)
-	self.label_Data_Sub.setText('######')
-	self.label_Data_ContP.setText(str_contP)
-	self.label_Data_Pub.setText('######')
+        global contS, contP, str_contS
+        contS=0
+        contP=0
+        str_contS=str(contS)
+        str_contP=str(contP)
+        self.label_Data_ContS.setText(str_contS)
+        self.label_Data_Sub.setText('######')
+        self.label_Data_ContP.setText(str_contP)
+        self.label_Data_Pub.setText('######')
 
     def publish(self):
-	global contP
-	contP=contP+1
-	str_contP=str(contP)
-	pub=rospy.Publisher('ROSgui_published', String, queue_size=10)
-	hello_gui="Hello from GUI"
-	pub.publish(hello_gui)
-	self.label_Data_ContP.setText(str_contP)
-	self.label_Data_Pub.setText(hello_gui)
+        global contP
+        contP=contP+1
+        str_contP=str(contP)
+        pub=rospy.Publisher('ROSgui_published', String, queue_size=10)
+        hello_gui="Hello from GUI"
+        pub.publish(hello_gui)
+        self.label_Data_ContP.setText(str_contP)
+        self.label_Data_Pub.setText(hello_gui)
 
 	
 if __name__ == "__main__":
-    import sys
-    app = QtWidgets.QApplication(sys.argv)
-    MainWindow = QtWidgets.QMainWindow()
-    ui = Ui_MainWindow()
-    ui.setupUi(MainWindow)
-    MainWindow.show()
-    
-    sys.exit(app.exec_())
+        import sys
+        app = QtWidgets.QApplication(sys.argv)
+        MainWindow = QtWidgets.QMainWindow()
+        ui = Ui_MainWindow()
+        ui.setupUi(MainWindow)
+        MainWindow.show()
+
+        sys.exit(app.exec_())
 
